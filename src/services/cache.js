692f3bca353b7d6181402e2af3de28323fb7ba64
Axel Dolce <axel.dolce@gmail.com>
2017-02-17 00:28:16 -0300

    bypass cache & set cached on response

--- src/services/cache.js
+++ src/services/cache.js
@@ -2,60 +2,57 @@ import NodeCache from 'node-cache'
 
 import Response from './response'
 
-export const NAME = 'CACHE/CACHE'
-export const GET = 'CACHE/GET'
-export const SET = 'CACHE/SET'
-export const OPTIONS = 'CACHE/OPTIONS'
-export const STATS = 'CACHE/STATS'
+export const NAME = 'CACHE'
+export const GET = 'GET'
+export const SET = 'SET'
+export const OPTIONS = 'OPTIONS'
+export const STATS = 'STATS'
 
-export const patterns = (function(){
+export const patterns = (function () {
 
     var patterns = {
-        echo : {
-            service : NAME
-        },
+        echo: {},
         set: {
-            service: NAME,
-            type: SET
+            TYPE: SET
         },
-        get : {
-            service: NAME,
-            type: GET
+        get: {
+            TYPE: GET
         },
-        options : {
-            service : NAME,
-            type : OPTIONS
+        options: {
+            TYPE: OPTIONS
         },
-        stats : {
-            service : NAME,
-            type : STATS
+        stats: {
+            TYPE: STATS
         }
     }
+    Object.keys(patterns).map(name => {
+        Object.assign(patterns[name], { SERVICE: NAME })
+    })
     return patterns
 
 })();
 
 
 
-(function actionCreators(exports){
-    exports.get = function get(namespace, key) {
+var actions = {
+    get(namespace, key) {
         return Object.assign({}, patterns.get, { namespace, key })
-    }
-    exports.set = function set(namespace, key, value) {
+    },
+    set(namespace, key, value) {
         return Object.assign({}, patterns.set, { namespace, key, value })
-    }
-    exports.options = function options(namespace, options={}){
+    },
+    options(namespace, options = {}) {
         return Object.assign({}, patterns.options, {
-            stdTTL : options.stdTTL
+            stdTTL: options.stdTTL
         })
-    }
-    exports.stats = function stats(namespace){
+    },
+    stats(namespace) {
         return Object.assign({}, patterns.stats, {
             namespace
         })
     }
-})(exports);
-
+}
+Object.assign(exports, actions)
 
 
 export default function cache(options = {}) {
@@ -69,7 +66,7 @@ export default function cache(options = {}) {
     this.add(patterns.get, (action, done) => {
 
         var { namespace, key } = action
-        
+
         console.info('GET', namespace, key)
 
         var cache = resolveCache(namespace)
@@ -77,16 +74,16 @@ export default function cache(options = {}) {
         var cached = cache.get(key)
 
         done(null, {
-            data : cached
+            data: cached
         })
 
     })
     this.add(patterns.set, (action, done) => {
-        
+
         var { namespace, key, value } = action
 
         console.info('SET', namespace, key)
-        
+
         set(namespace, key, value)
 
         done(null)
@@ -112,36 +109,36 @@ export default function cache(options = {}) {
         done(null, res(true))
 
     })
-    this.add(patterns.stats, function onStats(action, done){
+    this.add(patterns.stats, function onStats(action, done) {
 
         console.info('STATS', STATS)
-        
+
         var stats = Object.keys(caches).map(namespace => {
             return {
-                [namespace] : {
-                    stats : caches[namespace].getStats(),
-                    keys : caches[namespace].keys()
+                [namespace]: {
+                    stats: caches[namespace].getStats(),
+                    keys: caches[namespace].keys().sort()
                 }
             }
         }).reduce((e1, e2) => Object.assign(e1, e2), {})
-        
+
         done(null, res(stats))
 
     })
 
-    function set(namespace, key, value){
+    function set(namespace, key, value) {
         resolveCache(namespace)
         caches[namespace].set(key, value)
     }
-    function resolveCache(namespace){
+    function resolveCache(namespace) {
         if (caches[namespace]) {
             return caches[namespace]
         }
         caches[namespace] = new NodeCache({
-            stdTTL : 60,
-            checkperiod : 5
+            stdTTL: 60,
+            checkperiod: 5
         })
-        caches[namespace].on('expired', function(key, value){
+        caches[namespace].on('expired', function (key, value) {
             console.log('key %s expired', key)
             console.log(caches[namespace].getStats())
         })
@@ -152,9 +149,9 @@ export default function cache(options = {}) {
 
 
 
-export function useCache(options = {}){
+export function useCache(options = {}) {
 
-    var debug = options.log || function(){}
+    var debug = options.log || function () { }
 
     var seneca = this
 
@@ -172,21 +169,31 @@ export function useCache(options = {}){
         seneca.add(PIN.pin, _useCache)
     })
 
-    function _useCache(action, done){
+    function _useCache(action, done) {
 
         var seneca = this
-        
-        var cache_key = JSON.stringify(seneca.util.clean(action))
+
+        var cache_key = seneca.util.pattern(seneca.util.clean(action))
+
+        if (typeof action.useCache !== 'undefined' && !action.useCache) {
+            return seneca.prior(action, function(err, response){
+                cache.set(cache_key, response).catch(err => {
+                    debug('cache.set %s', err.name)
+                })
+                done(err, response)
+            })
+        }
 
         var getCached = cache.get(cache_key).catch(err => {
             debug('cache.get %s', err.name)
             return null
         })
-        
+
         getCached.then(data => {
-            
+
             if (data) {
                 debug('hit %s', cache_key)
+                data.cache = true
                 return done(null, data)
             }
 
@@ -205,8 +212,8 @@ export function useCache(options = {}){
     }
 
     seneca.add({
-        init : 'useCache'
-    }, function(action, done){
+        init: 'useCache'
+    }, function (action, done) {
         done()
     })
 
@@ -216,13 +223,13 @@ export function useCache(options = {}){
 
 
 
-export function CacheClient(seneca, namespace){
-    
+export function CacheClient(seneca, namespace) {
+
     if (!namespace) throw new Error('CacheClientError')
 
     var self = this
     this.set = (key, value) => {
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             var action = exports.set(namespace, key, value)
             seneca.act(action, (err, response) => {
                 err ? reject(err) : resolve(response)
@@ -230,7 +237,7 @@ export function CacheClient(seneca, namespace){
         })
     }
     this.get = (key) => {
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             var action = exports.get(namespace, key)
             seneca.act(action, (err, response) => {
                 err ? reject(err) : resolve(response.data)
@@ -238,7 +245,7 @@ export function CacheClient(seneca, namespace){
         })
     }
     this.options = (options) => {
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             var action = exports.options(namespace, options)
             seneca.act(action, (err, response) => {
                 err ? reject(err) : resolve(response)
